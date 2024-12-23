458e6e50fa52b82ab99b9d9ddea2f95c17c8c63e
Veskeli <veskeli12@gmail.com>
2024-12-24 00:00:31 +0200

    Add Settings tab and corresponding component; refactor App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,26 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import "./App.css";
 import HomeTab from "./HomeTab";
+import Settings from "./Settings";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
     <main className="container">
       <h1>Welcome to Coffee powered tools!</h1>
 
-      <Tabs>
-        <TabList>
-          <Tab>Home</Tab>
-          <Tab>About</Tab>
-        </TabList>
-
-        <TabPanel>
-          <div className="row">
-            <a href="https://vitejs.dev" target="_blank">
-              <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-            </a>
-            <a href="https://tauri.app" target="_blank">
-              <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-            </a>
-            <a href="https://reactjs.org" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
-          <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-          <form
-            className="row"
-            onSubmit={(e) => {
-              e.preventDefault();
-              greet();
-            }}
-          >
-            <input
-              id="greet-input"
-              onChange={(e) => setName(e.currentTarget.value)}
-              placeholder="Enter a name..."
-            />
-            <button type="submit">Greet</button>
-          </form>
-          <p>{greetMsg}</p>
-        </TabPanel>
-        <TabPanel>
-          <HomeTab />
-        </TabPanel>
+      <Tabs selectedTabClassName="react-tabs__tab--selected">
+          <TabList>
+            <Tab>Home</Tab>
+            <Tab>Settings</Tab>
+          </TabList>
+            <TabPanel>
+              <HomeTab />
+            </TabPanel>
+            <TabPanel>
+              <Settings />
+            </TabPanel>
       </Tabs>
     </main>
   );
---
+++ src/Settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div>
+      <h2>Settings</h2>
+      <p>This is the settings page. You can add your settings here.</p>
+    </div>
+  );
+};
+
+export default Settings;

897a125bddc5586854ba4cffa046a27498f3e841
Veskeli <veskeli12@gmail.com>
2024-12-23 23:27:39 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import "./App.css";
+import HomeTab from "./HomeTab";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <main className="container">
+      <h1>Welcome to Coffee powered tools!</h1>
+
+      <Tabs>
+        <TabList>
+          <Tab>Home</Tab>
+          <Tab>About</Tab>
+        </TabList>
+
+        <TabPanel>
+          <div className="row">
+            <a href="https://vitejs.dev" target="_blank">
+              <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+            </a>
+            <a href="https://tauri.app" target="_blank">
+              <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+            </a>
+            <a href="https://reactjs.org" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+          <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+          <form
+            className="row"
+            onSubmit={(e) => {
+              e.preventDefault();
+              greet();
+            }}
+          >
+            <input
+              id="greet-input"
+              onChange={(e) => setName(e.currentTarget.value)}
+              placeholder="Enter a name..."
+            />
+            <button type="submit">Greet</button>
+          </form>
+          <p>{greetMsg}</p>
+        </TabPanel>
+        <TabPanel>
+          <HomeTab />
+        </TabPanel>
+      </Tabs>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/HomeTab.tsx
@@ -0,0 +1,15 @@
+// HomeTab.tsx
+import React from 'react';
+
+const HomeTab = () => (
+  <div className="row">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+    </a>
+    <a href="https://tauri.app" target="_blank">
+      <img src="/images/coffee3.png" className="logo coffee" alt="coffee logo" />
+    </a>
+  </div>
+);
+
+export default HomeTab;
